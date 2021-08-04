ad4463735860e91e13e379d98161dad931c7f331
Sabrina Smai <71526914+sabrina-smai@users.noreply.github.com>
2021-08-03 22:06:30 -0700

    Update the PyTorch Profiler version to 1.9
    Updated v1.8 mention to v1.9

--- intermediate_source/tensorboard_profiler_tutorial.py
+++ intermediate_source/tensorboard_profiler_tutorial.py
@@ -6,7 +6,7 @@ to detect performance bottlenecks of the model.
 
 Introduction
 ------------
-PyTorch 1.8 includes an updated profiler API capable of 
+PyTorch 1.9 includes an updated profiler API capable of 
 recording the CPU side operations as well as the CUDA kernel launches on the GPU side.
 The profiler can visualize this information
 in TensorBoard Plugin and provide analysis of the performance bottlenecks.
